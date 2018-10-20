442ba0b9e4fe19aa627d01289ec9af03b400d5f8
unknown <savinraul1983@yahoo.com>
2018-10-20 11:41:16 +0100

    axios request

--- src/components/currency/LiveCurrency.js
+++ src/components/currency/LiveCurrency.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react'
 import SearchCurrency from './SearchCurrency';
 import Currency from './Currency';
+import axios from 'axios';
 
 class LiveCurrency extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currency: {}
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://data.fixer.io/api/latest?access_key=b0ab4945712b358052a8fc54d02e7b3d&base=EUR&symbols=USD,CAD')
+      .then(res => {
+        const response = res.data;
+        this.setState({ currency: response })
+      })
+  }
   render() {
     const test = ['test1', 'test2', 'test3'];
 
