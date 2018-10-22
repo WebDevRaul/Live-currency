a055e5d01f7ae8149953c7e807b9cfc41275d729
unknown <savinraul1983@yahoo.com>
2018-10-22 11:44:21 +0100

    onChange input

--- src/components/currency/SearchCurrency.js
+++ src/components/currency/SearchCurrency.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
 
 class SearchCurrency extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      error: {}
+    }
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+  };
+
+  onSubmit(e) {
+    e.preventDefault()
+    console.log(this.state.text)
+    this.setState({ text: '' })
+  }
+
+  onChange(e) {
+    this.setState({ text: e.target.value })
+  }
+
   render() {
     return (
       <div>
-        <form>
-          <input />
-          <button>Search</button>
+        <form onSubmit={this.onSubmit}>
+          <input
+            type='text'
+            name='text'
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <button type='submit'>Search</button>
         </form>
       </div>
     )
