f525762fcc96e780b8ea42e6d443f28caac9aa49
unknown <savinraul1983@yahoo.com>
2018-10-25 23:53:21 +0100

    in progres

--- src/Test.js
+++ src/Test.js
@@ -17,7 +17,21 @@ export default class Test extends Component {
 
   onSubmit(e) {
     e.preventDefault();
-    console.log(this.state.input)
+
+    const { text } = this.state;
+
+    if(text.trim().length > 3) {
+      if (text.indexOf(',') > -1) {
+        const x = text.trim().split(',')
+        console.log(x)
+        if (x.map(e => e).length > 3) {
+          console.log('second is greater then 3')
+        }
+      }
+      console.log('greater then 3')
+    } else {
+      console.log('else')
+    }
   } 
 
   render() {
