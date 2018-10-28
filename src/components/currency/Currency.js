385a5cd06f854350ab16adecd0d06a00cbcad976
unknown <savinraul1983@yahoo.com>
2018-10-28 11:46:10 +0000

    quick currency css

--- src/components/currency/Currency.js
+++ src/components/currency/Currency.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+//Css
+import '../css/Currency.css';
 
 export default class Currency extends Component {
   render() {
     return (
-      <div>
-        <p>{this.props.name}: <span>{this.props.data}</span></p>
+      <div className='row'>
+        <div className='col'>
+          <div className='currency'>
+            <p className='currencyName'>{this.props.name} : <span>{this.props.data}</span></p>
+          </div>
+        </div>
       </div>
     )
   }
