b3572164cc097127890352f30f1d7075e9bd82d5
unknown <savinraul1983@yahoo.com>
2018-10-29 18:08:29 +0000

    a quick link to live-currency

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Landing extends Component {
   render() {
     return (
       <div>
-      Landing
+        <Link to='/live-currency'>Live Currency</Link>
       </div>
     )
   }
