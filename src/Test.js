07b81da28e86966d821f7afafa3c62369edb2765
unknown <savinraul1983@yahoo.com>
2018-11-01 19:22:49 +0000

    before more change push to heroku for meeting

--- src/Test.js
+++ src/Test.js
@@ -98,3 +98,19 @@ export default class Test extends Component {
     )
   }
 }
+
+    // .then(res => {
+    //   const date = res.payload.date;
+    //   const today = new Date(date).toISOString().slice(0, 10);
+    //   const yesterday = ( today => new Date(today.setDate(today.getDate() - 1)) )(new Date(date)).toISOString().slice(0, 10);
+    //   axios
+    //     .get(`https://api.exchangeratesapi.io/history?start_at=${yesterday}&end_at=${yesterday}&symbols=${newRates}&base=${base}`)
+    //     .then(res => dispatch({
+    //       type: GET_YESTERDAY_RATE,
+    //       payload: res.data.rates
+    //     }))
+    //     .catch(err => dispatch({
+    //       type: GET_ERRORS,
+    //       payload: err.response.data
+    //     }))
+    // })
