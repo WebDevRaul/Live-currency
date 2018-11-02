2a39e169bc158e7ceea840152a595360248e08b8
unknown <savinraul1983@yahoo.com>
2018-11-02 15:50:59 +0000

    get yesterday basicRates

--- src/redux/actions/getBasicRates.js
+++ src/redux/actions/getBasicRates.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_BASIC_RATES, GET_ERRORS } from './types';
+import { GET_BASIC_RATES, GET_ERRORS, GET_YESTERDAY_RATE } from './types';
 
 //import Loading action
 import { setLoading } from './commonAction';
@@ -12,6 +12,21 @@ export const getBasicRates = (base, symbols) => dispatch => {
       type: GET_BASIC_RATES,
       payload: res.data
     }))
+    .then(res => {
+      const date = res.payload.date;
+      const today = new Date(date).toISOString().slice(0, 10);
+      const yesterday = ( today => new Date(today.setDate(today.getDate() - 1)) )(new Date(date)).toISOString().slice(0, 10);
+      axios
+        .get(`https://api.exchangeratesapi.io/history?start_at=${yesterday}&end_at=${yesterday}&symbols=${symbols}&base=${base}`)
+        .then(res => dispatch({
+          type: GET_YESTERDAY_RATE,
+          payload: res.data.rates
+        }))
+        .catch(err => dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        }))
+    })
     .catch(err => dispatch({
       type: GET_ERRORS,
       payload: err.response.data
