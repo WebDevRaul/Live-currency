a9f07f2cc3c372607a29aabbea0c3d21b6ee600f
unknown <savinraul1983@yahoo.com>
2018-11-13 10:21:33 +0000

    multiple single newRate request added to the array

--- src/redux/reducers/get_New_Symbols.js
+++ src/redux/reducers/get_New_Symbols.js
@@ -11,7 +11,7 @@ export default function(state=initialState, action) {
       return {
         ...state,
         loading: false,
-        newSymbols: action.payload
+        newSymbols: [...state.newSymbols, action.payload]
       }
     default:
       return state;
