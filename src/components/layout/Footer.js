bbdba143394ef7350b3eb797338a3fe7d0e3717e
unknown <savinraul1983@yahoo.com>
2018-12-03 15:48:43 +0000

    footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -7,7 +7,38 @@ class Footer extends Component {
   render() {
     return (
       <div className='footer'>
-        Footer
+      <hr />
+        <div className='row footerDiv'>
+          <div className='col footerLogo'>
+            <span >Copyright &copy; {new Date().getFullYear()} Live-Currency</span>
+          </div>
+          <div className='col'>
+            <div className='footer-links'>
+              <p>FIND US ON</p>
+              <span>
+                <a href='https://www.facebook.com/' target='_blank' rel="noopener noreferrer">
+                  <i className='fab fa-facebook-f'></i>
+                </a>
+              </span>
+              <span>
+                <a href='https://twitter.com/' target='_blank' rel="noopener noreferrer">
+                  <i className='fab fa-twitter'></i>
+                </a>
+              </span>
+              <span>
+                <a href='https://www.instagram.com/' target='_blank' rel="noopener noreferrer">
+                  <i className='fab fa-instagram'></i>
+                </a>
+              </span>
+              <span>
+                <a href='https://www.linkedin.com/' target='_blank' rel="noopener noreferrer">
+                  <i className='fab fa-linkedin-in'></i>
+                </a>
+              </span>
+            </div>
+          </div>
+        </div>
+        <hr />
       </div>
     )
   }
