d0ac8f6f30f6589e78593aa26ffcb3ca30ae6100
unknown <savinraul1983@yahoo.com>
2018-12-05 09:28:17 +0000

    Logo

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+//Components
+import Logo from './Logo';
+
 //Css
 import '../css/Navbar.css';
 
@@ -8,6 +11,7 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light">
+        <Logo />
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
