c352afcf627ba734f6c175b3acbc6653885142c8
unknown <savinraul1983@yahoo.com>
2018-12-12 11:18:37 +0000

    footer is more responsive now

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -11,33 +11,49 @@ class Footer extends Component {
     return (
       <div className='footer'>
         <hr />
-        <div className='row footerDiv'>
-          <div className='col-md-6 col-sm-12 col footerLogo'>
-            <span >Copyright &copy; {new Date().getFullYear()} <Logo /></span>
-          </div>
-          <div className='col-md-6 col-sm-12'>
-            <div className='footer-links'>
-              <p>FIND US ON</p>
-              <span>
-                <a href='https://www.facebook.com/' target='_blank' rel="noopener noreferrer">
-                  <i className='fab fa-facebook-f'></i>
-                </a>
-              </span>
-              <span>
-                <a href='https://twitter.com/' target='_blank' rel="noopener noreferrer">
-                  <i className='fab fa-twitter'></i>
-                </a>
-              </span>
-              <span>
-                <a href='https://www.instagram.com/' target='_blank' rel="noopener noreferrer">
-                  <i className='fab fa-instagram'></i>
-                </a>
-              </span>
-              <span>
-                <a href='https://www.linkedin.com/' target='_blank' rel="noopener noreferrer">
-                  <i className='fab fa-linkedin-in'></i>
-                </a>
-              </span>
+        <div className='footer-Div'>
+          <div className='row'>
+            <div className='col col-sm-6 footer-Logo'>
+              <div className='row'>
+                <div className='col'>
+                  <div className=''>
+                    <span >Copyright &copy; {new Date().getFullYear()} <Logo /></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='col col-sm-6 footer-Info'>
+              <div className='row'>
+                <div className='col'>
+                  <p>FIND US ON</p>
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col'>
+                  <div className='footer-Links'>
+                    <span>
+                      <a href='https://www.facebook.com/' target='_blank' rel="noopener noreferrer">
+                        <i className='fab fa-facebook-f'></i>
+                      </a>
+                    </span>
+                    <span>
+                      <a href='https://twitter.com/' target='_blank' rel="noopener noreferrer">
+                        <i className='fab fa-twitter'></i>
+                      </a>
+                    </span>
+                    <span>
+                      <a href='https://www.instagram.com/' target='_blank' rel="noopener noreferrer">
+                        <i className='fab fa-instagram'></i>
+                      </a>
+                    </span>
+                    <span>
+                      <a href='https://www.linkedin.com/' target='_blank' rel="noopener noreferrer">
+                        <i className='fab fa-linkedin-in'></i>
+                      </a>
+                    </span>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
