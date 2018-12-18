9a7c3778dc979c8aeab33c2c39a88e4c48d17c68
TextError <savinraul1983@yahoo.com>
2018-12-18 12:02:15 +0000

    api call dispatch new msg on empty response

--- src/redux/actions/get_New_Rate.js
+++ src/redux/actions/get_New_Rate.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_NEW_RATE_TODAY, GET_ERRORS, GET_NEW_RATE_YESTERDAY, GET_NEW_RATE_LAST_YEAR, GET_EXCHANGE_NEW_TODAY } from './types';
+import { GET_NEW_RATE_TODAY, GET_ERRORS, GET_NEW_RATE_YESTERDAY, GET_NEW_RATE_LAST_YEAR, GET_EXCHANGE_NEW_TODAY, GET_EXCHANGE_NEW_YESTERDAY, GET_EXCHANGE_NEW_LAST_YEAR } from './types';
 
 //import Loading action
 import { setNewRateLoading } from './commonAction';
@@ -11,10 +11,19 @@ export const get_New_Rate = (base, date, newSymbols) => dispatch => {
   dispatch(setNewRateLoading());
     axios
       .get(`https://api.exchangeratesapi.io/latest?base=${base}&symbols=${newSymbols}`)
-      .then(res => dispatch({
-        type: GET_NEW_RATE_TODAY,
-        payload: res.data.rates
-      }))
+      .then(res => {
+        if (isEmpty(res.data.rates)) {
+          dispatch({
+            type: GET_EXCHANGE_NEW_TODAY,
+            payload: 'Exchange Rate Closed'
+          })
+        } else {
+          dispatch({
+            type: GET_NEW_RATE_TODAY,
+            payload: res.data.rates
+          })
+        }
+      })
       .catch(err => dispatch({
         type: GET_ERRORS,
         payload: err.response.data
@@ -22,32 +31,37 @@ export const get_New_Rate = (base, date, newSymbols) => dispatch => {
     axios
       .get(`https://api.exchangeratesapi.io/history?start_at=${date.yesterday}&end_at=${date.yesterday}&symbols=${newSymbols}&base=${base}`)
       .then(res => {
-        if (!isEmpty(res.data.rates)) {
+        if (isEmpty(res.data.rates)) {
+          dispatch({
+            type: GET_EXCHANGE_NEW_YESTERDAY,
+            payload:'Exchange Rate Closed'
+          })
+        } else {
           dispatch({
             type: GET_NEW_RATE_YESTERDAY,
             payload: Object.keys(res.data.rates).map(i => res.data.rates[i])[0]
           })
-        } else {
-          dispatch({
-            type: GET_EXCHANGE_NEW_TODAY,
-            payload:'Exchange Rate Closed'
-          })
         }
       })
-      // .then(res => dispatch({
-      //   type: GET_NEW_RATE_YESTERDAY,
-      //   payload: Object.keys(res.data.rates).map(i => res.data.rates[i])[0]
-      // }))
       .catch(err => dispatch({
         type: GET_ERRORS,
         payload: err.response.data
       }))
     axios
       .get(`https://api.exchangeratesapi.io/history?start_at=${date.lastYear}&end_at=${date.lastYear}&symbols=${newSymbols}&base=${base}`)
-      .then(res => dispatch({
-        type: GET_NEW_RATE_LAST_YEAR,
-        payload: Object.keys(res.data.rates).map(i => res.data.rates[i])[0]
-      }))
+        .then(res => {
+          if (isEmpty(res.data.rates)) {
+            dispatch({
+              type: GET_EXCHANGE_NEW_LAST_YEAR,
+              payload: 'Exchange Rate Closed'
+            })
+          } else {
+            dispatch({
+              type: GET_NEW_RATE_LAST_YEAR,
+              payload: Object.keys(res.data.rates).map(i => res.data.rates[i])[0]
+            })
+          }
+        })
       .catch(err => dispatch({
         type: GET_ERRORS,
         payload: err.response.data
