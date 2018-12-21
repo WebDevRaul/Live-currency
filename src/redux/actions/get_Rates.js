2f7b9f38ecf15718f040145d722c11c543148440
TextError <savinraul1983@yahoo.com>
2018-12-21 11:22:26 +0000

    reset

--- src/redux/actions/get_Rates.js
+++ src/redux/actions/get_Rates.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_TODAY, GET_DATE, GET_YESTERDAY, GET_LAST_YEAR, GET_ERRORS,GET_FAKE_DATE, GET_EXCHANGE_YESTERDAY, GET_EXCHANGE_LAST_YEAR } from './types';
+import { GET_TODAY, GET_YESTERDAY, GET_LAST_YEAR, GET_ERRORS, GET_EXCHANGE_YESTERDAY, GET_EXCHANGE_LAST_YEAR } from './types';
 
 //import Loading action
 import { setRateLoading } from './commonAction';
@@ -21,10 +21,20 @@ export const get_Rates = (base, date, symbols) => dispatch => {
   //GET_YESTERDAY Rates
   axios
     .get(`https://api.exchangeratesapi.io/history?start_at=${date.yesterday}&end_at=${date.yesterday}&symbols=${symbols}&base=${base}`)
-    .then(res => dispatch({
-      type: GET_YESTERDAY,
-      payload: Object.keys(res.data.rates).map(i => res.data.rates[i])[0]
-    }))
+    .then(res => {
+      //If_Empty YESTERDAY Rates
+      if (isEmpty(res.data.rates)) {
+        dispatch({
+          type: GET_EXCHANGE_YESTERDAY,
+          payload: 'Exchange Rate Closed'
+        })
+      } else {
+        dispatch({
+          type: GET_YESTERDAY,
+          payload: Object.keys(res.data.rates).map(i => res.data.rates[i])[0]
+        })
+      }
+    })
     .catch(err => dispatch({
       type: GET_ERRORS,
       payload: err.response.data
@@ -32,13 +42,22 @@ export const get_Rates = (base, date, symbols) => dispatch => {
   //GET_LAST_YEAR Rates
   axios
     .get(`https://api.exchangeratesapi.io/history?start_at=${date.lastYear}&end_at=${date.lastYear}&symbols=${symbols}&base=${base}`)
-    .then(res => dispatch({
-      type: GET_LAST_YEAR,
-      payload: Object.keys(res.data.rates).map(i => res.data.rates[i])[0]
-
-    }))
+    .then(res => {
+      //If_EMPTY LAST_YEAR Rates
+      if (isEmpty(res.data.rates)) {
+        dispatch({
+          type: GET_EXCHANGE_LAST_YEAR,
+          payload: 'Exchange Rate Closed'
+        })
+      } else {
+        dispatch({
+          type: GET_LAST_YEAR,
+          payload: Object.keys(res.data.rates).map(i => res.data.rates[i])[0]
+        })
+      }
+    })
     .catch(err => dispatch({
       type: GET_ERRORS,
       payload: err.response.data
-    }))
+    }));
 };
--- src/redux/reducers/get_Date.js
+++ src/redux/reducers/get_Date.js
@@ -1,4 +1,4 @@
-import { GET_DATE, GET_FAKE_DATE, GET_LOADING } from '../actions/types';
+import { GET_DATE, GET_LOADING } from '../actions/types';
 
 const initialState = {
   loading: false,
@@ -21,32 +21,12 @@ export default function(state = initialState, action) {
       const yesterday = ( today => new Date(today.setDate(today.getDate() - 1)) )(new Date(action.payload)).toISOString().slice(0, 10);
       const LastYear = new Date(action.payload).getFullYear() - 1;
       const lastYear = ( today => new Date(today.setFullYear(LastYear)) )(new Date(action.payload)).toISOString().slice(0, 10);
-
-      const twoDaysBeforeToday = ( today => new Date(today.setDate(today.getDate() - 2)) )(new Date(action.payload)).toISOString().slice(0, 10);
       return {
         ...state,
         loading: false,
         today: today,
         yesterday: yesterday,
         lastYear: lastYear,
-        twoDaysBeforeToday: twoDaysBeforeToday,
-      }
-    case GET_FAKE_DATE:
-      const todayTwo = new Date(action.payload).toISOString().slice(0,10);
-      
-      const oneDayBeforeYesterday = ( todayTwo => new Date(todayTwo.setDate(todayTwo.getDate() - 1)) )(new Date(todayTwo)).toISOString().slice(0, 10);
-      console.log(oneDayBeforeYesterday, 'today')
-
-      const twoDaysBeforeYesterday = ( todayTwo => new Date(todayTwo.setDate(todayTwo.getDate() - 2)) )(new Date(todayTwo)).toISOString().slice(0, 10);
-      
-      const oneDayBeforeLastYear = ( todayTwo => new Date(todayTwo.setFullYear(LastYear)) )(new Date(oneDayBeforeYesterday)).toISOString().slice(0, 10);
-      
-      return {
-        ...state,
-        loading: false,
-        oneDayBeforeYesterday: oneDayBeforeYesterday,
-        twoDaysBeforeYesterday: twoDaysBeforeYesterday,
-        oneDayBeforeLastYear: oneDayBeforeLastYear
       }
     default:
       return state;
