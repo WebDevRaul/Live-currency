1ea88fb3085f4fb3771941ca9c1d0a227e6d0979
TextError <savinraul1983@yahoo.com>
2018-12-21 11:13:17 +0000

    basic call

--- src/redux/actions/get_Rates.js
+++ src/redux/actions/get_Rates.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_TODAY, GET_DATE, GET_YESTERDAY, GET_LAST_YEAR, GET_ERRORS, GET_EXCHANGE_YESTERDAY, GET_EXCHANGE_LAST_YEAR } from './types';
+import { GET_TODAY, GET_DATE, GET_YESTERDAY, GET_LAST_YEAR, GET_ERRORS,GET_FAKE_DATE, GET_EXCHANGE_YESTERDAY, GET_EXCHANGE_LAST_YEAR } from './types';
 
 //import Loading action
 import { setRateLoading } from './commonAction';
@@ -7,149 +7,36 @@ import isEmpty from '../../components/common/isEmpty';
 
 export const get_Rates = (base, date, symbols) => dispatch => {
   dispatch(setRateLoading());
+  //GET_TODAY Rates
   axios
     .get(`https://api.exchangeratesapi.io/latest?base=${base}&symbols=${symbols}`)
-    // .get(`https://api.exchangeratesapi.io/history?start_at=2018-12-16&end_at=2018-12-16&symbols=${symbols}&base=${base}`)
-    .then(res => {
-      if (isEmpty(res.data.rates)) {
-        console.log('is Empty' )
-        //Change Date
-        axios
-          .get(`https://api.exchangeratesapi.io/history?start_at=${date.yesterday}&end_at=${date.yesterday}&symbols=GBP&base=EUR`)
-          .then(res => dispatch({
-            type: GET_DATE,
-            payload: res.data.start_at
-          }))
-          .catch(err => dispatch({
-            type: GET_ERRORS,
-            payload: err.response.data
-          }))
-          //GET Today if Empty
-        axios
-          .get(`https://api.exchangeratesapi.io/history?start_at=${date.yesterday}&end_at=${date.yesterday}&symbols=${symbols}&base=${base}`)
-          .then(res => {
-            if (isEmpty(res.data.rates)) {
-              //Change Date
-              axios
-                .get(`https://api.exchangeratesapi.io/history?start_at=${date.oneDayBeforeYesterday}&end_at=${date.oneDayBeforeYesterday}&symbols=GBP&base=EUR`)
-                .then(res => dispatch({
-                  type: GET_DATE,
-                  payload: res.data.start_at
-                }))
-                .catch(err => dispatch({
-                  type: GET_ERRORS,
-                  payload: err.response.data
-                }))
-              //Get Today if first Today is Empty
-              axios
-                .get(`https://api.exchangeratesapi.io/history?start_at=${date.oneDayBeforeYesterday}&end_at=${date.oneDayBeforeYesterday}&symbols=${symbols}&base=${base}`)
-                .then(res => dispatch({
-                  type: GET_TODAY,
-                  payload: Object.values(res.data.rates)[0]
-                }))
-                .catch(err => dispatch({
-                  type: GET_ERRORS,
-                  payload: err.response.data
-                }))
-              //Get Yesterday for Today is first Today is Empty
-              axios
-                .get(`https://api.exchangeratesapi.io/history?start_at=${date.twoDaysBeforeYesterday}&end_at=${date.twoDaysBeforeYesterday}&symbols=${symbols}&base=${base}`)
-                .then(res => dispatch({
-                  type: GET_YESTERDAY,
-                  payload: Object.values(res.data.rates)[0]
-                }))
-                .catch(err => dispatch({
-                  type: GET_ERRORS,
-                  payload: err.response.data
-                }))
-            } else {
-              dispatch({
-                type: GET_TODAY,
-                payload: Object.values(res.data.rates)[0]
-              })
-            }
-          })
-          .catch(err => dispatch({
-            type: GET_ERRORS,
-            payload: err.response.data
-          }))
-        //Yesterday for Today
-        axios
-          .get(`https://api.exchangeratesapi.io/history?start_at=${date.oneDayBeforeYesterday}&end_at=${date.oneDayBeforeYesterday}&symbols=${symbols}&base=${base}`)
-          .then(res => dispatch({
-            type: GET_YESTERDAY,
-            payload: Object.values(res.data.rates)[0]
-          }))
-          .catch(err => dispatch({
-            type: GET_ERRORS,
-            payload: err.response.data
-          }))
-        //LastYear for Today
-        axios
-          .get(`https://api.exchangeratesapi.io/history?start_at=${date.oneDayBeforeLastYear}&end_at=${date.oneDayBeforeLastYear}&symbols=${symbols}&base=${base}`)
-          .then(res => {
-            if (isEmpty(res.data.rates)) {
-              dispatch({
-                type: GET_EXCHANGE_LAST_YEAR,
-                payload: 'Exchange Rate Closed'
-              })
-            } else {
-              dispatch({
-                type: GET_LAST_YEAR,
-                payload: Object.keys(res.data.rates).map(i => res.data.rates[i])[0]
-              })
-            }
-          })
-          .catch(err => dispatch({
-            type: GET_ERRORS,
-            payload: err.response.data
-          }))
-      } else {
-        dispatch({
-          type: GET_TODAY,
-          payload: res.data.rates
-          // payload: Object.values(res.data.rates)[0]
-        })
-        axios
-        .get(`https://api.exchangeratesapi.io/history?start_at=${date.yesterday}&end_at=${date.yesterday}&symbols=${symbols}&base=${base}`)
-        .then(res => {
-          if (isEmpty(res.data.rates)) {
-            dispatch({
-              type: GET_EXCHANGE_YESTERDAY,
-              payload: 'Exchange Rate Closed'
-            })
-          } else {
-            dispatch({
-              type: GET_YESTERDAY,
-              payload: Object.keys(res.data.rates).map(i => res.data.rates[i])[0]
-            })
-          }
-        })
-        .catch(err => dispatch({
-          type: GET_ERRORS,
-          payload: err.response.data
-        }))
-      axios
-        .get(`https://api.exchangeratesapi.io/history?start_at=${date.lastYear}&end_at=${date.lastYear}&symbols=${symbols}&base=${base}`)
-        .then(res => {
-          if (isEmpty(res.data.rates)) {
-            dispatch({
-              type: GET_EXCHANGE_LAST_YEAR,
-              payload: 'Exchange Rate Closed'
-            })
-          } else {
-            dispatch({
-              type: GET_LAST_YEAR,
-              payload: Object.keys(res.data.rates).map(i => res.data.rates[i])[0]
-            })
-          }
-        })
-        .catch(err => dispatch({
-          type: GET_ERRORS,
-          payload: err.response.data
-        }))
-      }
-    })
+    .then(res => dispatch({
+      type: GET_TODAY,
+      payload: res.data.rates
+    }))
+    .catch(err => dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    }));
+  //GET_YESTERDAY Rates
+  axios
+    .get(`https://api.exchangeratesapi.io/history?start_at=${date.yesterday}&end_at=${date.yesterday}&symbols=${symbols}&base=${base}`)
+    .then(res => dispatch({
+      type: GET_YESTERDAY,
+      payload: Object.keys(res.data.rates).map(i => res.data.rates[i])[0]
+    }))
+    .catch(err => dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    }));
+  //GET_LAST_YEAR Rates
+  axios
+    .get(`https://api.exchangeratesapi.io/history?start_at=${date.lastYear}&end_at=${date.lastYear}&symbols=${symbols}&base=${base}`)
+    .then(res => dispatch({
+      type: GET_LAST_YEAR,
+      payload: Object.keys(res.data.rates).map(i => res.data.rates[i])[0]
+
+    }))
     .catch(err => dispatch({
       type: GET_ERRORS,
       payload: err.response.data
--- src/redux/reducers/get_Date.js
+++ src/redux/reducers/get_Date.js
@@ -1,4 +1,4 @@
-import { GET_DATE, GET_LOADING } from '../actions/types';
+import { GET_DATE, GET_FAKE_DATE, GET_LOADING } from '../actions/types';
 
 const initialState = {
   loading: false,
@@ -21,20 +21,26 @@ export default function(state = initialState, action) {
       const yesterday = ( today => new Date(today.setDate(today.getDate() - 1)) )(new Date(action.payload)).toISOString().slice(0, 10);
       const LastYear = new Date(action.payload).getFullYear() - 1;
       const lastYear = ( today => new Date(today.setFullYear(LastYear)) )(new Date(action.payload)).toISOString().slice(0, 10);
+
+      const twoDaysBeforeToday = ( today => new Date(today.setDate(today.getDate() - 2)) )(new Date(action.payload)).toISOString().slice(0, 10);
       return {
         ...state,
         loading: false,
         today: today,
         yesterday: yesterday,
         lastYear: lastYear,
+        twoDaysBeforeToday: twoDaysBeforeToday,
       }
     case GET_FAKE_DATE:
-    
-      const oneDayBeforeYesterday = ( today => new Date(today.setDate(today.getDate() - 2)) )(new Date(action.payload)).toISOString().slice(0, 10);
-      const twoDaysBeforeYesterday = ( today => new Date(today.setDate(today.getDate() - 3)) )(new Date(action.payload)).toISOString().slice(0, 10);
+      const todayTwo = new Date(action.payload).toISOString().slice(0,10);
       
-      const oneDayBeforeLastYear = ( today => new Date(today.setFullYear(LastYear)) )(new Date(oneDayBeforeYesterday)).toISOString().slice(0, 10);
+      const oneDayBeforeYesterday = ( todayTwo => new Date(todayTwo.setDate(todayTwo.getDate() - 1)) )(new Date(todayTwo)).toISOString().slice(0, 10);
+      console.log(oneDayBeforeYesterday, 'today')
 
+      const twoDaysBeforeYesterday = ( todayTwo => new Date(todayTwo.setDate(todayTwo.getDate() - 2)) )(new Date(todayTwo)).toISOString().slice(0, 10);
+      
+      const oneDayBeforeLastYear = ( todayTwo => new Date(todayTwo.setFullYear(LastYear)) )(new Date(oneDayBeforeYesterday)).toISOString().slice(0, 10);
+      
       return {
         ...state,
         loading: false,
