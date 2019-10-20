e4c6edbd60d0b9607c4d3de7e71baa5a55883727
TextError <savinraul1983@yahoo.com>
2019-10-20 11:37:36 +0300

    insert EUR: 1 for convertor when you chose base EUR

--- src/redux/actions/utils/convertor.js
+++ src/redux/actions/utils/convertor.js
@@ -24,21 +24,27 @@ export const GET = ({
     try {
       dispatch({ type: LOADING, payload: true });
       const response = await axios.get(`${ENDPOINT}&base=${BASE}`);
-      const success = response.data;
+      const data = response.data;
+      // If base is EUR insert EUR: 1, api do not return data for EUR
+      const success = BASE === 'EUR' ? {...data, rates: {...data.rates, EUR: 1}} : data;
       // If response is empty
       if(await isEmpty(success.rates)) {
         const response = await axios.get(`${HISTORY}start_at=${DAY_BEFORE}&end_at=${DAY_BEFORE}&base=${BASE}`);
         const payload = response.data;
         const { start_at, base } = payload;
         const rates = Object.values(payload.rates)[0];
-        const success = { date: start_at, rates , base }
+        const data = { date: start_at, rates , base };
+        // If base is EUR insert EUR: 1, api do not return data for EUR
+        const success = BASE === 'EUR' ? {...data, rates: {...data.rates, EUR: 1}} : data;
         // if response is empty
         if(await isEmpty(rates)) {
           const response = await axios.get(`${HISTORY}start_at=${TWO_DAYS_BEFORE}&end_at=${TWO_DAYS_BEFORE}&base=${BASE}`);
           const payload = response.data;
           const { start_at, base } = payload;
           const rates = Object.values(payload.rates)[0];
-          const success = { date: start_at, rates , base }
+          const data = { date: start_at, rates , base }
+          // If base is EUR insert EUR: 1, api do not return data for EUR
+          const success = BASE === 'EUR' ? {...data, rates: {...data.rates, EUR: 1}} : data;
           return onSuccess(success);
         }
         return onSuccess(success);
